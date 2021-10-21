be786506d8f1012f569c49f62a61dbbc77295a75
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-21 07:05:15 +0530

    Update lightningrun.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/TeamFussion/Dark_Fussion /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/kaal0408/LUCIFER_BOT /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
